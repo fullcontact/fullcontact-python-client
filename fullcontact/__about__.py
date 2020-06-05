be3decdf914c073048ee26b53ddcf6e6ed0325e2
Nithin Subhash <nithinsubhash003@gmail.com>
2020-06-05 12:41:33 +0530

    Minor edit to app description

--- fullcontact/__about__.py
+++ fullcontact/__about__.py
@@ -12,5 +12,5 @@ __version__ = "2.0.0"
 __user_agent__ = "FullContact_Python_Client_V%s" % __version__
 __author__ = "FullContact"
 __author_email__ = "pypy@fullcontact.com"
-__description__ = "A simple FullContact API client library"
+__description__ = "Client library for FullContact V3 Enrich and Resolve APIs"
 __license__ = "Apache License, Version 2.0"
